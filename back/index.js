4f429471a4e4b2a1dfa14a1bebc78fc7419286eb
Martina del Pino <martudelpino@gmail.com>
2024-03-21 16:14:02 +0100

    configuracion backend lista

---
+++ back/index.js
@@ -0,0 +1,10 @@
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+const axios = require("axios")
+
+// Syncing all the models at once.
+conn.sync({ force: true }).then(async() => {
+  server.listen({ port: process.env.PORT || 3001 }, () => {
+    console.log('%s listening at 3000 ESTOY DENTRO'); // eslint-disable-line no-console
+  });
+});
